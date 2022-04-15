70201938ea581f7d2b1b833a8293955d2e4bbeb2
Bryan Simbaña <bryan-_1997@outlook.com>
2022-04-14 23:50:32 -0500

    Add login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './Components/login/login.component';
 import { NewUserComponent } from './Components/new-user/new-user.component';
 
 const routes: Routes = [
-  {path: 'nuevo-usuario',component: NewUserComponent}
+  { path: 'login', component: LoginComponent, pathMatch: 'full' },
+  { path: 'nuevo-usuario', component: NewUserComponent },
+  { path: '**', redirectTo: 'login' }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NewUserComponent } from './Components/new-user/new-user.component';
+import { LoginComponent } from './Components/login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NewUserComponent
+    NewUserComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
