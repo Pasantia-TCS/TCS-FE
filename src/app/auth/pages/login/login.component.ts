9c30cb647e6c0d9931a1c40b2c8e8afb53adefa2
Bryan Simbaña <bryan-_1997@outlook.com>
2022-04-26 01:44:36 -0500

    Add frontend validations

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -19,12 +19,21 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  login() {
-    // out: {ultimatix: '', password: ''}
-    console.log(this.myForm.value);
-    console.log(this.myForm.valid);
+  valid_field(field_name: string) {
+    return this.myForm.controls[field_name].errors && this.myForm.controls[field_name].touched;
+  }
 
-    this.router.navigateByUrl('/pages/selection');
+  login() {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    } else {
+      // out: {ultimatix: '', password: ''}
+      console.log(this.myForm.value);
+      console.log(this.myForm.valid);
+      this.router.navigateByUrl('/pages/selection');
+      this.myForm.reset();
+    }
   }
 
 }
--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -11,11 +11,11 @@ export class RegisterComponent implements OnInit {
 
   myForm: FormGroup = this.fb.group({
     name: ['', [Validators.required, Validators.maxLength(30)]],
-    lastname: ['', [Validators.required, , Validators.maxLength(30)]],
-    phone: ['', [Validators.required], Validators.minLength(10), Validators.maxLength(10)],
-    email: ['', [Validators.required], Validators.email],
+    lastname: ['', [Validators.required, Validators.maxLength(30)]],
+    phone: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(10), Validators.pattern("(09)[8-9]{1}[0-9]{7}")]],
+    email: ['', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]],
     ultimatix: ['', [Validators.required]],
-    password: ['', [Validators.required]]
+    password: ['', [Validators.required, Validators.minLength(8)]]
   });
 
   constructor(private fb: FormBuilder, private router: Router) { }
@@ -23,10 +23,32 @@ export class RegisterComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  register() {
-    console.log(this.myForm.value);
-    console.log(this.myForm.valid);
+  get email() {
+    return this.myForm.get('email');
+  }
 
-    this.router.navigateByUrl('/pages/selection');
+  get phone() {
+    return this.myForm.get('phone');
+  }
+
+  get password() {
+    return this.myForm.get('password');
+  }
+
+  valid_field(field_name: string) {
+    return this.myForm.controls[field_name].errors && this.myForm.controls[field_name].touched;
+  }
+
+  register() {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    } else {
+      // out: {ultimatix: '', password: ''}
+      console.log(this.myForm.value);
+      console.log(this.myForm.valid);
+      this.router.navigateByUrl('/pages/selection');
+      this.myForm.reset();
+    }
   }
 }
