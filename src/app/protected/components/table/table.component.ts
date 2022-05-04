eff9edb9f3a329aa9c47ce899331737ef9944be5
Renan Andres Garcia <andresgarcia17@icloud.com>
2022-05-04 17:40:50 -0500

    add table

---
+++ src/app/protected/components/table/table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+//import { Product } from '../../domain/product';
+//import { ProductService } from '../../service/productservice';
+
+@Component({
+    selector: 'p-table',
+    templateUrl: './table.component.html'
+})
+export class TableBasic implements OnInit {
+
+    constructor() { }
+
+    ngOnInit(): void {
+    }  
+}
