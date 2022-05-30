51753d486d7b7187943aeac8f362df9f9d92c2a7
Bryan Simbaña <bryan-_1997@outlook.com>
2022-05-30 11:20:01 -0500

    Opciones de administrador

--- src/app/protected/components/dashboard/dashboard.component.ts
+++ src/app/protected/components/dashboard/dashboard.component.ts
@@ -3,6 +3,8 @@ import { Router } from '@angular/router';
 import { AuthService } from 'src/app/auth/services/auth.service';
 import { User } from 'src/app/auth/interfaces/user';
 import { UserService } from 'src/app/shared/services/user.service';
+import { Profile } from '../../interfaces/profile';
+import { ProfileService } from '../../services/profile.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -13,8 +15,10 @@ export class DashboardComponent implements OnInit {
 
   currentUser!: User;
   collapse = true;
+  profile!: Profile;
 
   constructor(
+    private profileService: ProfileService,
     private userService: UserService,
     private authService: AuthService,
     private router: Router
@@ -22,6 +26,12 @@ export class DashboardComponent implements OnInit {
 
   ngOnInit(): void {
     this.currentUser = this.userService.getUserData();
+
+    // Load profile info
+    this.profileService.getProfile(this.currentUser.id_numero_Ultimatix!)
+      .subscribe({
+        next: resp => this.profile = resp
+      });
   }
 
   toggleSidebar() {
