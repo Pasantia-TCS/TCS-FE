974fe2657645d02441ac77383129b16eb5047cbf
Bryan Simbaña <bryan-_1997@outlook.com>
2022-06-02 15:02:36 -0500

    Filtrar usuarios

--- src/app/protected/components/settings/settings.component.ts
+++ src/app/protected/components/settings/settings.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, PipeTransform } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatTableDataSource } from '@angular/material/table';
 import { Subscription } from 'rxjs';
 import { User } from 'src/app/auth/interfaces/user';
 import Swal from 'sweetalert2';
@@ -14,27 +15,14 @@ import { SettingsService } from '../../services/settings.service';
 
 export class SettingsComponent implements OnInit {
 
-  currentUser: User = {}
-  ultimatix: string | undefined = '';
+  displayedColumns: string[] = ['actions', 'id_ultimatix', 'nombres_completos', 'rol'];
+  dataSource!: MatTableDataSource<Profile>;
 
-  users!: Profile[];
-  tableHeader: string[] = ['Acciones', 'Ultimatix', 'Nombre', 'Rol'];
+  filterForm: FormGroup = this.fb.group({
+    searchItem: ['', Validators.required],
+  });
 
-
-  pipe!: PipeTransform;
-  filterForm: FormGroup = this.fb.group(
-    {
-      searchItem: ['', Validators.required],
-    }
-  );
-
-  clickEventSubscription: Subscription;
-
-  constructor(private settingsService: SettingsService, private fb: FormBuilder) {
-    this.clickEventSubscription = this.settingsService.getClickEvent()
-      .subscribe(() => setTimeout(() => this.loadUsers(), 500));
-
-  }
+  constructor(private settingsService: SettingsService, private fb: FormBuilder) { }
 
   ngOnInit(): void {
     this.loadUsers()
@@ -43,14 +31,11 @@ export class SettingsComponent implements OnInit {
   loadUsers() {
     this.settingsService.getUsers()
       .subscribe({
-        next: resp => {
-          this.users = resp
-        }
+        next: resp => this.dataSource = new MatTableDataSource(resp)
       })
   }
 
   update(id_ultimatix: number) {
-
     Swal.fire({
       title: '¿Estás seguro que deseas cambiar el rol de este usuario?',
       icon: 'warning',
@@ -62,33 +47,15 @@ export class SettingsComponent implements OnInit {
     }).then((result) => {
       if (result.isConfirmed) {
         this.settingsService.changeRole(id_ultimatix)
-          .subscribe(
-            {
-              next: resp => {
-                this.loadUsers();
-                Swal.fire('El rol de este usuario ha cambiado!', resp.mensaje, 'success');
-              },
-              error: err => Swal.fire('Error', err.error.mensaje, 'error')
-            }
-          );
+          .subscribe({
+            next: resp => {
+              this.loadUsers();
+              Swal.fire('¡El rol de este usuario ha cambiado!', resp.mensaje, 'success');
+            },
+            error: err => Swal.fire('Error', err.error.mensaje, 'error')
+          });
       }
     });
-
-
   }
-
-  /*
-  search(): User[] {
-    
-    const { searchItem } = this.filterForm.value;
-    console.log(searchItem);
-  
-    return this.users.filter(user => {
-      return this.pipe.transform(user.nombre).includes(searchItem.toLowerCase())
-          || this.pipe.transform(user.apellido).includes(searchItem.toLowerCase())
-          || this.pipe.transform(user.rol).includes(searchItem.toLowerCase());
-    });
-  }
-  */
 
 }
