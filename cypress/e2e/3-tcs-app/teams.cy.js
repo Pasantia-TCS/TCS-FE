2b882ebc4cdfdd000f7dc3949b235763f6539083
gabyz <gaby.zumarragad@gmail.com>
2022-06-10 14:28:27 -0500

    Guiones de prueba - Grupos de trabajo

--- cypress/e2e/3-tcs-app/teams.cy.js
+++ cypress/e2e/3-tcs-app/teams.cy.js
@@ -6,6 +6,7 @@ Cypress.Commands.add('login', (username, password) => {
   cy.wait(500)
 })
 
+
 describe('Ingreso a la pantalla de grupos de trabajo desde un usuario administrador', () => {
 
   // Ingreso con usuario admin
@@ -20,10 +21,11 @@ describe('Ingreso a la pantalla de grupos de trabajo desde un usuario administra
   it('Test 2: El titulo es correcto y la lista de los grupos de trabajo se visualiza', () => {
     cy.get('#btnTeams').click()
     cy.get('#teamsTitle').should('have.text', 'Grupos de trabajo')
-    //cy.get('#tableEquipos').should('have.data', '{}')
+    cy.get('#tableEquipos tr').should('have.length.at.least', 1)
   });
 
 });
+
 
 describe('Intento de ingreso a la pantalla de grupos de trabajo desde un usuario no administrador', () => {
 
@@ -61,39 +63,239 @@ describe('Agregar nuevo grupo de trabajo', () => {
     const teamType = 'Proyecto'
 
     cy.get('#btnTeams').click()
-    cy.get('#addTeam').click()
-    cy.get('#floatingTeamName').type(teamName)
-    cy.get('#floatingTeamType').select(teamType)
-    cy.get('#floatingTeamLeader').type(teamLeader)
-    cy.get('#floatingTeamTech').type(teamTech)
-    cy.get('#floatingTeamDesc').type(teamDescription)
-    cy.get('#btnSave').click()
+    cy.wait(1000)
+    cy.get('#tableEquipos tr')
+      .then(listing => {
+        const itemcount = Cypress.$(listing).length
+
+        cy.get('#addTeam').click()
+        cy.get('#floatingTeamName').type(teamName)
+        cy.get('#floatingTeamType').select(teamType)
+        cy.get('#floatingTeamLeader').type(teamLeader)
+        cy.get('#floatingTeamTech').type(teamTech)
+        cy.get('#floatingTeamDesc').type(teamDescription)
+        cy.get('#btnSave').click()
+        cy.contains('Éxito')
+        cy.contains('OK').click()
+        cy.get('#tableEquipos tr').should('have.length', (itemcount + 1))
+      });
+
   });
 
   it('Test 2: Agregar un nuevo grupo de trabajo de tipo "Celula"', () => {
     const teamType = 'Célula'
 
     cy.get('#btnTeams').click()
-    cy.get('#addTeam').click()
-    cy.get('#floatingTeamName').type(teamName)
-    cy.get('#floatingTeamType').select(teamType)
-    cy.get('#floatingTeamLeader').type(teamLeader)
-    cy.get('#floatingTeamTech').type(teamTech)
-    cy.get('#floatingTeamDesc').type(teamDescription)
-    cy.get('#btnSave').click()
+    cy.wait(1000)
+    cy.get('#tableEquipos tr')
+      .then(listing => {
+        const itemcount = Cypress.$(listing).length
+
+        cy.get('#addTeam').click()
+        cy.get('#floatingTeamName').type(teamName)
+        cy.get('#floatingTeamType').select(teamType)
+        cy.get('#floatingTeamLeader').type(teamLeader)
+        cy.get('#floatingTeamTech').type(teamTech)
+        cy.get('#floatingTeamDesc').type(teamDescription)
+        cy.get('#btnSave').click()
+        cy.contains('Éxito')
+        cy.contains('OK').click()
+        cy.get('#tableEquipos tr').should('have.length', (itemcount + 1))
+      });
   });
 
   it('Test 3: Agregar un nuevo grupo de trabajo de tipo "Tribu"', () => {
     const teamType = 'Tribu'
 
     cy.get('#btnTeams').click()
-    cy.get('#addTeam').click()
-    cy.get('#floatingTeamName').type(teamName)
-    cy.get('#floatingTeamType').select(teamType)
-    cy.get('#floatingTeamLeader').type(teamLeader)
-    cy.get('#floatingTeamTech').type(teamTech)
-    cy.get('#floatingTeamDesc').type(teamDescription)
-    cy.get('#btnSave').click()
+    cy.wait(1000)
+    cy.get('#tableEquipos tr')
+      .then(listing => {
+        const itemcount = Cypress.$(listing).length
+
+        cy.get('#addTeam').click()
+        cy.get('#floatingTeamName').type(teamName)
+        cy.get('#floatingTeamType').select(teamType)
+        cy.get('#floatingTeamLeader').type(teamLeader)
+        cy.get('#floatingTeamTech').type(teamTech)
+        cy.get('#floatingTeamDesc').type(teamDescription)
+        cy.get('#btnSave').click()
+        cy.contains('Éxito')
+        cy.contains('OK').click()
+        cy.get('#tableEquipos tr').should('have.length', (itemcount + 1))
+      });
   });
 
 });
+
+
+describe('Intentar agregar un nuevo grupo de trabajo duplicado', () => {
+
+  // Ingreso con usuario admin
+  beforeEach(() => {
+    cy.login('2255136', 'P@ssw0rd')
+  });
+
+  const teamName = 'TCS Project'
+  const teamLeader = 'Wilman Remache'
+  const teamTech = 'Mauricio Bayas'
+  const teamDescription = 'Aplicativo para uso administrativo de TCS.'
+
+  it('Test 1: Intentar agregar un nuevo grupo de trabajo de tipo "Proyecto"', () => {
+    const teamType = 'Proyecto'
+
+    cy.get('#btnTeams').click()
+    cy.wait(1000)
+    cy.get('#tableEquipos tr')
+      .then(listing => {
+        const itemcount = Cypress.$(listing).length
+
+        cy.get('#addTeam').click()
+        cy.get('#floatingTeamName').type(teamName)
+        cy.get('#floatingTeamType').select(teamType)
+        cy.get('#floatingTeamLeader').type(teamLeader)
+        cy.get('#floatingTeamTech').type(teamTech)
+        cy.get('#floatingTeamDesc').type(teamDescription)
+        cy.get('#btnSave').click()
+        cy.contains('Error')
+        cy.contains('OK').click()
+        cy.get('#tableEquipos tr').should('have.length', (itemcount))
+      });
+
+  });
+
+  it('Test 2: Intentar agregar un nuevo grupo de trabajo de tipo "Celula"', () => {
+    const teamType = 'Célula'
+
+    cy.get('#btnTeams').click()
+    cy.wait(1000)
+    cy.get('#tableEquipos tr')
+      .then(listing => {
+        const itemcount = Cypress.$(listing).length
+
+        cy.get('#addTeam').click()
+        cy.get('#floatingTeamName').type(teamName)
+        cy.get('#floatingTeamType').select(teamType)
+        cy.get('#floatingTeamLeader').type(teamLeader)
+        cy.get('#floatingTeamTech').type(teamTech)
+        cy.get('#floatingTeamDesc').type(teamDescription)
+        cy.get('#btnSave').click()
+        cy.contains('Error')
+        cy.contains('OK').click()
+        cy.get('#tableEquipos tr').should('have.length', (itemcount))
+      });
+  });
+
+  it('Test 3: Intentar agregar un nuevo grupo de trabajo de tipo "Tribu"', () => {
+    const teamType = 'Tribu'
+
+    cy.get('#btnTeams').click()
+    cy.wait(1000)
+    cy.get('#tableEquipos tr')
+      .then(listing => {
+        const itemcount = Cypress.$(listing).length
+
+        cy.get('#addTeam').click()
+        cy.get('#floatingTeamName').type(teamName)
+        cy.get('#floatingTeamType').select(teamType)
+        cy.get('#floatingTeamLeader').type(teamLeader)
+        cy.get('#floatingTeamTech').type(teamTech)
+        cy.get('#floatingTeamDesc').type(teamDescription)
+        cy.get('#btnSave').click()
+        cy.contains('Error')
+        cy.contains('OK').click()
+        cy.get('#tableEquipos tr').should('have.length', (itemcount))
+      });
+  });
+});
+
+
+describe('Editar un grupo de trabajo', () => {
+
+  // Ingreso con usuario admin
+  beforeEach(() => {
+    cy.login('2255136', 'P@ssw0rd')
+  });
+
+  const teamLeader = 'Jimmy Zumarraga'
+  const teamTech = 'Dhiraj Bellara'
+
+  it('Test 1: Editar un grupo de trabajo', () => {
+
+    cy.get('#btnTeams').click()
+    cy.wait(1000)
+    cy.get('#tableEquipos tr')
+      .then(listing => {
+        const itemcount = Cypress.$(listing).length
+
+        cy.contains('TCS Project').parents('tr').find('button').contains('edit').click()
+        cy.get('#floatingTeamName').should('be.disabled')
+        cy.get('#floatingTeamType').should('be.disabled')
+        cy.get('#floatingTeamLeader').clear().type(teamLeader)
+        cy.get('#floatingTeamTech').clear().type(teamTech)
+        cy.get('#floatingTeamDesc').should('be.disabled')
+        cy.get('#btnSave').click()
+        cy.contains('Éxito')
+        cy.contains('OK').click()
+        cy.get('#tableEquipos tr').should('have.length', (itemcount))
+
+      });
+  });
+});
+
+
+describe('Eliminar un grupo de trabajo', () => {
+
+  // Ingreso con usuario admin
+  beforeEach(() => {
+    cy.login('2255136', 'P@ssw0rd')
+  });
+
+  it('Test 1: Eliminar un grupo de trabajo', () => {
+
+    cy.get('#btnTeams').click()
+    cy.wait(1000)
+    cy.get('#tableEquipos tr')
+      .then(listing => {
+        const itemcount = Cypress.$(listing).length
+
+        cy.contains('TCS Project').parents('tr').find('button').contains('delete').click()
+        cy.contains('Sí').click()
+        cy.contains('Éxito')
+        cy.contains('OK').click()
+        cy.contains('TCS Project').parents('tr').find('button').contains('add_box')
+        //cy.contains('TCS Project').parents('tr').contains('No Vigente')
+        cy.get('#tableEquipos tr').should('have.length', (itemcount))
+
+      });
+  });
+});
+
+
+describe('Restablecer un grupo de trabajo', () => {
+
+  // Ingreso con usuario admin
+  beforeEach(() => {
+    cy.login('2255136', 'P@ssw0rd')
+  });
+
+  it('Test 1: Restablecer un grupo de trabajo', () => {
+
+    cy.get('#btnTeams').click()
+    cy.wait(1000)
+    cy.get('#tableEquipos tr')
+      .then(listing => {
+        const itemcount = Cypress.$(listing).length
+
+        cy.contains('TCS Project').parents('tr').find('button').contains('add_box').click()
+        cy.contains('Sí').click()
+        cy.contains('Éxito')
+        cy.contains('OK').click()
+        cy.contains('TCS Project').parents('tr').find('button').contains('delete')
+        //cy.contains('TCS Project').parents('tr').contains('Vigente')
+        cy.get('#tableEquipos tr').should('have.length', (itemcount))
+
+      });
+  });
+});
+
